88b57a460c2bf4e890e11027648605e92a399f8a
noyer8 <contact@noyer.io>
2025-11-23 00:01:26 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import { instagramPost } from "./instagram.js";
 
 const app = express();
 app.use(express.json());
 
+// ================================
+// ROUTES
+// ================================
+app.post("/instagram/post", instagramPost);
+
+// ================================
 app.get("/", (req, res) => {
   res.send("Noyer META Backend is running 🚀");
 });
